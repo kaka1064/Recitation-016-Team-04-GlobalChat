9ad462856af869357f2b91eb2a0b58084bf9eff2
Andrew Pleeter <95309613+anpl1623@users.noreply.github.com>
2023-04-16 15:21:33 -0600

    Update index.js
    login and register api's

--- AllProjectCode/index.js
+++ AllProjectCode/index.js
@@ -44,6 +44,66 @@ app.get('/welcome', (req, res) => {
 app.get('/login',(req,res)=>{
     res.render('views/pages/login');
 });
+app.post("/register", async (req,res) => {
+  const username = req.body.username;
+  const hash = await bcrypt.hash(req.body.password, 10);
+  const query = `insert into users (username, password) values ($1, $2) returning $1;`; 
+  const values = [username, hash];
+  db.one(query,values).then(data=>{
+    res.render("views/pages/login",{
+      message: `${username}'s account created successfully`,
+      error: false
+    });
+  }).catch(error=>{
+    res.render("views/pages/login",{
+      message: `ERROR: ${error}`,
+      error: true
+    });
+  });
+});
+
+app.post("/login", (req,res)=> {
+  const username = req.body.username;
+  const query = `select * from users where username = $1;`;
+  const list = [username];
+  db.one(query,list).then(temp=>{
+    const match = bcrypt.compare(req.body.password, temp.password,(error,result)=>{
+      if (result && !error){
+        req.session.save();
+        res.redirect("/home");
+      }
+      else if (error) res.render("views/pages/login",{message:error, error: true});
+      else res.redirect("/home");
+    });
+  }).catch(error=>{
+    res.render("views/pages/login",{
+      message: "Invalid username/password",
+      error: true
+    });
+  });
+});
+
+app.get("/logout",(req,res)=>{
+  user.username = null;
+  user.password = null;
+  req.session.destroy();
+  res.render("views/pages/login", {
+    message: "Log out successful",
+    error: false
+  });
+});
+
+// Authentication Middleware.
+const auth = (req, res, next) => {
+  if (!req.session.user) {
+    // Default to login page.
+    return res.redirect('/login');
+  }
+  next();
+};
+
+// Authentication Required
+app.use(auth);
 
 module.exports = app.listen(3000);
 console.log("success");
