7f0fe3050fc22c4af3677fa1bc20cb173b56153b
Joseph Westley <jowe6894@gmail.com>
2023-04-19 21:01:10 -0600

    pushing to temp branch

--- AllProjectCode/test/server.spec.js
+++ AllProjectCode/test/server.spec.js
@@ -33,22 +33,22 @@ describe('Server!', () => {
       .end((err, res) => {
         console.log(res);
         expect(res).to.have.status(200);
-        //expect(res.body.message).to.equals('Success');
+        expect(res.body.message).to.equals('Success');
         done();
       });
   });
 
-  // //We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 200 along with a "Invalid input" message.
-  // it('Negative : /login. Checking invalid name', done => {
-  //   chai
-  //     .request(server)
-  //     .post('/login')
-  //     .send({username: 'jdfgh', password: 'aaaaa'})
-  //     .end((err, res) => {
-  //       expect(res).to.have.status(400);
-  //       expect(res.body.message).to.equals('Invalid input');
-  //       done();
-  //     });
-  //});
+  //We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 200 along with a "Invalid input" message.
+  it('Negative : /login. Checking invalid name', done => {
+    chai
+      .request(server)
+      .post('/login')
+      .send({username: 'jdfgh', password: 'aaaaa'})
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.message).to.equals('Invalid input');
+        done();
+      });
+  });
 
 });
